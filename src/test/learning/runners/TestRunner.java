9ce339d7cfadbedd1bb3f223632f07f5c4bf3fe1
qimei <qimei@192.168.0.10>
2017-03-03 13:49:24 +0800

    tidy up files and added step definition after testrunner run

---
+++ src/test/learning/runners/TestRunner.java
@@ -0,0 +1,17 @@
+package test.learning.runners;
+
+import org.junit.runner.RunWith;
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+ 
+//@RunWith tells JUnit that tests should run using Cucumber class present in ‘Cucumber.api.junit‘ package
+@RunWith(Cucumber.class)
+//@CucumberOptions tells Cucumber where to look for feature files, what reporting system to use and some other things
+@CucumberOptions(
+		// folder location for all .features files
+		features = "classpath:test/features"
+		,glue={"classpath:test/stepDefinitions"}
+		)
+public class TestRunner {
+
+}
